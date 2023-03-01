cdf7bfbe2a88ebd7c844390273f2fb6d7b2cdc61
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-03-01 17:08:00 +0100

    add <Switch>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import {NavLink, Redirect, Route, Switch} from "react-router-dom";
+
 function App() {
   return (
-    <>App</>
+    <div>
+      <h1>
+        APPLICATION WITH NAVIGATION
+      </h1>
+      <NavLink to={"/users"}>USERS LIST</NavLink>
+      <Switch>
+        <Route path='/users' component={UsersListPage} />
+        <Route path='/' component={HomePage} />
+        <Redirect to='/' />
+      </Switch>
+    </div>
   );
 }
 
 export default App;
+
+
