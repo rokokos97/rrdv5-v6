a0f0f49303388c8d306793d2f9ef6cd9c2bfdae1
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-03-01 17:12:10 +0100

    add Users

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import {NavLink, Redirect, Route, Switch} from "react-router-dom";
+import {NavLink, Redirect, Route, Switch, useRouteMatch} from "react-router-dom";
 
 function App() {
-  return (
+    const Users = () => {
+        const { path } = useRouteMatch();
+        return (
+            <div>
+                <h1>USERS</h1>
+                <NavLink to='/'>Main Page</NavLink>
+                <Switch>
+                    <Route
+                        path={path + "/:userId/profile"}
+                        component={UserPage}
+                    />
+                    <Route path={path + "/:userId/edit"} component={EditUserPage} />
+                    <Route path={path} exact component={UserListPage} />
+                    <Redirect
+                        from={path + "/:userId"}
+                        to={path + "/:userId/profile"}
+                    />
+                </Switch>
+            </div>
+        );
+    }
+    const HomePage = () => <h1>HOME PAGE</h1>
+    return (
     <div>
       <h1>
         APPLICATION WITH NAVIGATION
       </h1>
-      <NavLink to={"/users"}>USERS LIST</NavLink>
+      <NavLink to={"/users"}>USERS</NavLink>
       <Switch>
-        <Route path='/users' component={UsersListPage} />
+        <Route path='/users' component={Users} />
         <Route path='/' component={HomePage} />
         <Redirect to='/' />
       </Switch>
